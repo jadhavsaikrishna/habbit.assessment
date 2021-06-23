6cc4358940406695cf0a67f972bc0701e53006a4
jadhav saikrishna <jadhavsaikrishna361@gmail.com>
2021-06-23 22:27:02 +0530

    habbit

---
+++ src/ListItems.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './ListItems.css'
+
+
+function ListItems(props){
+    const items = props.items;
+    const listItems = items.map(item =>
+        
+        {
+           return <div className="list" key={item.key}>
+               
+           
+           <p>{item.text}
+           <span>
+           <i class="fas fa-trash"
+           onClick={()=> props.deleteItem(item.key)}></i>
+           </span>
+           </p>
+            
+           </div>
+        }
+        )
+    return(
+        <div>{listItems}</div>
+    )
+}
+export default ListItems;
